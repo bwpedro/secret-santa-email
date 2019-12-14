93431e4e9171061ce86fd4af00524072fda490b7
bwpedro <pwarmling0@gmail.com>
2019-12-14 17:26:48 -0300

    little refactor

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
-  service: 'email-service', // gmail, yahoo
+  service: "email-service", // gmail, yahoo
   auth: {
-    user: 'youremail@example.com',
-    pass: 'password-example'
+    user: "youremail@example.com",
+    pass: "password-example"
   }
 });
 const map = new Map();
@@ -22,27 +22,30 @@ map.forEach((value, key) => {
   arrayEmail.push(value);
 });
 
-while(mapEmail.size != map.size){
+while (mapEmail.size != map.size) {
   const indexEmail = Math.floor(Math.random() * arrayEmail.length);
   const indexName = Math.floor(Math.random() * arrayName.length);
-  
-  if(!(map.get(arrayName[indexName]) == arrayEmail[indexEmail])){
-    if(!mapEmail.has(arrayEmail[indexEmail]) && !mapName.has(arrayName[indexName])){
+
+  if (map.get(arrayName[indexName]) != arrayEmail[indexEmail]) {
+    if (
+      !mapEmail.has(arrayEmail[indexEmail]) &&
+      !mapName.has(arrayName[indexName])
+    ) {
       mapEmail.set(arrayEmail[indexEmail], "");
       mapName.set(arrayName[indexName], "");
 
       var mailOptions = {
-        from: 'youremail@example.com',
+        from: "youremail@example.com",
         to: arrayEmail[indexEmail],
-        subject: 'Secret Sant',
+        subject: "Secret Sant",
         text: "Your secret santa is " + arrayName[indexName] + "!"
       };
 
-      transporter.sendMail(mailOptions, function(error, info){
+      transporter.sendMail(mailOptions, function(error, info) {
         if (error) {
           console.log(error);
         } else {
-          console.log('Email sent: ' + info.response);
+          console.log("Email sent: " + info.response);
         }
       });
     }
